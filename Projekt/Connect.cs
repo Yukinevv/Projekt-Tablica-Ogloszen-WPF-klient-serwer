25da662284c402b701b2d6af84abf740764a49ee
Adrian Rodzic <adrianrodzic33@gmail.com>
2022-04-04 16:53:36 +0200

    Dodaj pliki projektów.

---
+++ Projekt/Connect.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+
+namespace Projekt
+{
+    class Connect
+    {
+        public static void InsertRecord()
+        {
+            using (NpgsqlConnection con = GetConnection())
+            {
+                //string query = @"INSERT INTO public.emp(empno, ename, deptno) VALUES(4, 'Kowalski', 10)";
+                string query = @"INSERT INTO public.test VALUES(1, 'Jan', 'Kowalski', 123456789)";
+                NpgsqlCommand cmd = new NpgsqlCommand(query, con);
+                con.Open();
+                int n = cmd.ExecuteNonQuery();
+                if (n == 1)
+                {
+                    Console.WriteLine("Rekord zostal dodany do bazy");
+                }
+            }
+        }
+
+        public static List<string> SelectRecords()
+        {
+            List<string> result = new List<string>();
+            using (NpgsqlConnection con = GetConnection())
+            {      
+                string query = @"SELECT * FROM test";
+                NpgsqlCommand cmd = new NpgsqlCommand(query, con);
+                con.Open();
+                using (NpgsqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        //Console.WriteLine(reader.GetString(0));
+                        //Console.WriteLine(reader["id"] + " " + reader["imie"] + " " + reader["nazwisko"] + " " + reader["nr_tel"]);
+                        result.Add(reader["id"] + " " + reader["imie"] + " " + reader["nazwisko"] + " " + reader["nr_tel"]);
+                    }
+                }
+            }
+            return result;
+        }
+
+        public static void TestConnection()
+        {
+            using (NpgsqlConnection con = GetConnection())
+            {
+                con.Open();
+                if (con.State == System.Data.ConnectionState.Open)
+                {
+                    Console.WriteLine("Polaczono z baza danych");
+                }
+            }
+        }
+
+        private static NpgsqlConnection GetConnection()
+        {
+            return new NpgsqlConnection(@"Server=sxterm;Port=5432;User ID=adrianrodzic;Password=ADrian8151!@#;Database=adrianrodzic");
+        }
+    }
+}
---
+++ Projekt/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Projekt
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            textBlock1.Text = string.Empty;
+            foreach (var elements in Connect.SelectRecords())
+            {
+                textBlock1.Text += elements + "\n";
+            }
+        }
+    }
+}
