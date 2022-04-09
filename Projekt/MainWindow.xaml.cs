5b14281a31b38b769b448784a0e0920695e48f02
Adrian Rodzic <adrianrodzic33@gmail.com>
2022-04-09 19:03:06 +0200

    Rejestracja - INSERT do bazy

--- Projekt/MainWindow.xaml.cs
+++ Projekt/MainWindow.xaml.cs
@@ -104,5 +104,52 @@ namespace Projekt
                 TextBlock1.Text += elements + "\n";
             }
         }
+
+        private void Rejestruj_Click(object sender, RoutedEventArgs e)
+        {
+            using (NpgsqlConnection conn = Connect.GetConnection())
+            {
+                try
+                {
+                    conn.Open();
+                    string query = @"INSERT INTO uzytkownicy VALUES(
+                                     nextval('increment_id_uzytkownicy'), :_login, :_haslo, :_imie, :_nazwisko, :_data_ur, 'user')";
+                                    //increment_id_uzytkownicy jest to sekwencja robiaca za AUTO INCREMENT w postgresie
+
+                    NpgsqlCommand cmd = new NpgsqlCommand(query, conn);
+
+                    cmd.Parameters.AddWithValue("_login", TextBoxLogin.Text);     
+                    cmd.Parameters.AddWithValue("_haslo", PassBox1.Password);
+                    cmd.Parameters.AddWithValue("_imie", TextBoxImie.Text);
+                    cmd.Parameters.AddWithValue("_nazwisko", TextBoxNazwisko.Text);
+                    cmd.Parameters.AddWithValue("_data_ur", DatePicker1.SelectedDate);
+
+                    int n = cmd.ExecuteNonQuery();
+
+                    if (n == 1)
+                    { 
+                        TextBlock1.Visibility = (Visibility)0;
+                        TextBlock1.Text = "Rejestracja zakonczona powodzeniem!";
+                        //pokaz grid logowania
+                        logowanie.Visibility = (Visibility)0;
+                        rejestracja.Visibility = (Visibility)1;
+                    }
+                    else //jeszcze nie okodowane
+                    {
+                        MessageBox.Show("Podany login jest juz zajety", "Blad rejestracji", MessageBoxButton.OK, MessageBoxImage.Asterisk);
+                        conn.Close();
+                    }
+                }
+                catch (Exception err)
+                {
+                    MessageBox.Show("Blad: " + err.Message, "Cos poszlo nie tak", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+
+        private void PassBox2_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            LabelPass2.Visibility = (Visibility)1;
+        }
     }
 }
